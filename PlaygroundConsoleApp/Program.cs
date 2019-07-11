4202fed6219d0bee77f8ad0a850c2692ae35c6af
Alex Drachnev <alexander.drachnev@festo.com>
2019-07-11 14:57:17 +0200

    Add project files.

---
+++ PlaygroundConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaygroundConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Trigger();
+
+            
+
+
+            
+
+            Console.ReadLine();
+
+        }
+
+
+        static async void Trigger()
+        {
+            await InvertAsync(true).ContinueWith(x =>
+            {
+                Console.WriteLine(x.Result);
+            });
+
+            var res = await InvertAsync(false);
+            Console.WriteLine(res);
+        }
+
+        static async Task<bool> InvertAsync(bool value)
+        {
+            await Task.Run(() => value = !value);
+            return value;
+        }
+    }
+}
