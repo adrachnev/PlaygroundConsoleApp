8b24e3e3e43354e7c3185e55973f7dc776a42488
ldrv <alexander.drachnev@festo.com>
2023-03-13 14:54:33 +0100

    Placeholder string parsing

--- PlaygroundConsoleApp/Program.cs
+++ PlaygroundConsoleApp/Program.cs
@@ -27,21 +27,37 @@ namespace PlaygroundConsoleApp
             ParseModulePlaceholder();
         }
 
+        private static string SetSlotInIdForPlaceholder(string iconMarkup, string Guid) 
+        {
+            string placeholderProperty = "ModulePlaceHolder=\"";
+
+            var placeHolderPropertyIndex = iconMarkup.IndexOf(placeholderProperty);
+            if (placeHolderPropertyIndex == -1)
+                return iconMarkup;
+
+            var firstQuoteIndex = placeHolderPropertyIndex + placeholderProperty.Length;
+            var secondQuoteIndex = iconMarkup.IndexOf("\"", firstQuoteIndex);
+
+
+            var result = iconMarkup.Remove(firstQuoteIndex, secondQuoteIndex - firstQuoteIndex);
+
+            if (string.IsNullOrEmpty(Guid)) 
+            {
+                return result;
+            }
+            else 
+            {
+                return result.Insert(firstQuoteIndex, Guid);
+            }
+        }
         private static void ParseModulePlaceholder()
         {
             string iconMarkup = "<?xml version='1.0' encoding='UTF-8'?><Canvas Tag=\"VABA-S6-1-X5\" Width=\"142\" Height=\"145\" xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:attachedProperties=\"clr-namespace:Festo.Suite.Design.AttachedProperties;assembly=Festo.Suite.Design\" VerticalAlignment=\"Top\"><Canvas Tag=\"Spacer\" IsHitTestVisible=\"False\"><Rectangle Tag=\"Spacer-2\" Width=\"2\" Height=\"140\" Canvas.Left=\"140\" Canvas.Top=\"0\" /></Canvas><Canvas Tag=\"Valves\" IsHitTestVisible=\"False\"><Canvas Tag=\"Valve\"><Rectangle Tag=\"Fitting\" Width=\"10\" Height=\"5\" Fill=\"#FFB6BEC6\" Canvas.Left=\"76\" Canvas.Top=\"140\" /><Rectangle Tag=\"Bottom\" Width=\"16\" Height=\"84\" Fill=\"#FFD8DCE1\" Canvas.Left=\"73\" Canvas.Top=\"56\" /><Rectangle Tag=\"Top\" Width=\"16\" Height=\"56\" Fill=\"#FFB6BEC6\" Canvas.Left=\"73\" Canvas.Top=\"0\" /></Canvas><Canvas Tag=\"Valve\"><Rectangle Tag=\"Fitting-2\" Width=\"10\" Height=\"5\" Fill=\"#FFB6BEC6\" Canvas.Left=\"93\" Canvas.Top=\"140\" /><Rectangle Tag=\"Bottom-2\" Width=\"16\" Height=\"84\" Fill=\"#FFD8DCE1\" Canvas.Left=\"90\" Canvas.Top=\"56\" /><Rectangle Tag=\"Top-2\" Width=\"16\" Height=\"56\" Fill=\"#FFB6BEC6\" Canvas.Left=\"90\" Canvas.Top=\"0\" /></Canvas><Canvas Tag=\"Valve\"><Rectangle Tag=\"Fitting-3\" Width=\"10\" Height=\"5\" Fill=\"#FFB6BEC6\" Canvas.Left=\"110\" Canvas.Top=\"140\" /><Rectangle Tag=\"Bottom-3\" Width=\"16\" Height=\"84\" Fill=\"#FFD8DCE1\" Canvas.Left=\"107\" Canvas.Top=\"56\" /><Rectangle Tag=\"Top-3\" Width=\"16\" Height=\"56\" Fill=\"#FFB6BEC6\" Canvas.Left=\"107\" Canvas.Top=\"0\" /></Canvas><Canvas Tag=\"Valve\"><Rectangle Tag=\"Fitting-4\" Width=\"10\" Height=\"5\" Fill=\"#FFB6BEC6\" Canvas.Left=\"127\" Canvas.Top=\"140\" /><Rectangle Tag=\"Bottom-4\" Width=\"16\" Height=\"84\" Fill=\"#FFD8DCE1\" Canvas.Left=\"124\" Canvas.Top=\"56\" /><Rectangle Tag=\"Top-4\" Width=\"16\" Height=\"56\" Fill=\"#FFB6BEC6\" Canvas.Left=\"124\" Canvas.Top=\"0\" /></Canvas><Canvas Tag=\"Cut out\"><Path Tag=\"Middle\" Stroke=\"#FFF2F3F5\" StrokeThickness=\"7\" Canvas.Left=\"-208.10799999999995\" Canvas.Top=\"-2124.145\"><Path.Data><PathGeometry Figures=\"M316 2124.145c0 8.778-3.677 23.243-3.6 40.569.076 16.549 4.413 38.436 4.413 50.938 0 28.261-3.363 34.1-3.363 48.493\" /></Path.Data></Path><Path Tag=\"Right\" Stroke=\"#FFB6BEC6\" StrokeThickness=\"1\" Canvas.Left=\"-204.10799999999995\" Canvas.Top=\"-2124.145\"><Path.Data><PathGeometry Figures=\"M316 2124.145c0 8.778-3.677 23.243-3.6 40.569.076 16.549 4.413 38.436 4.413 50.938 0 28.261-3.363 34.1-3.363 48.493\" /></Path.Data></Path><Path Tag=\"Left\" Stroke=\"#FFB6BEC6\" StrokeThickness=\"1\" Canvas.Left=\"-212.10799999999995\" Canvas.Top=\"-2124.145\"><Path.Data><PathGeometry Figures=\"M316 2124.145c0 8.778-3.677 23.243-3.6 40.569.076 16.549 4.413 38.436 4.413 50.938 0 28.261-3.363 34.1-3.363 48.493\" /></Path.Data></Path></Canvas></Canvas><Rectangle Tag=\"Body\" Width=\"72\" Height=\"140\" Fill=\"#FFE5E8EB\" Canvas.Left=\"0\" Canvas.Top=\"0\" IsHitTestVisible=\"False\" /><Canvas Tag=\"Screws\" IsHitTestVisible=\"False\"><Ellipse Tag=\"Screw\" Width=\"7.0\" Height=\"7.0\" Fill=\"#FFB6BEC6\" Canvas.Left=\"2\" Canvas.Top=\"114\" /><Ellipse Tag=\"Screw-2\" Width=\"7.0\" Height=\"7.0\" Fill=\"#FFB6BEC6\" Canvas.Left=\"2\" Canvas.Top=\"126\" /><Ellipse Tag=\"Screw-3\" Width=\"7.0\" Height=\"7.0\" Fill=\"#FFB6BEC6\" Canvas.Left=\"63\" Canvas.Top=\"114\" /><Ellipse Tag=\"Screw-4\" Width=\"7.0\" Height=\"7.0\" Fill=\"#FFB6BEC6\" Canvas.Left=\"63\" Canvas.Top=\"19\" /><Ellipse Tag=\"Screw-5\" Width=\"7.0\" Height=\"7.0\" Fill=\"#FFB6BEC6\" Canvas.Left=\"2\" Canvas.Top=\"3\" /><Ellipse Tag=\"Screw-6\" Width=\"7.0\" Height=\"7.0\" Fill=\"#FFB6BEC6\" Canvas.Left=\"22\" Canvas.Top=\"7\" /></Canvas><Canvas Tag=\"LEDs\" IsHitTestVisible=\"False\"><Rectangle Tag=\"LED_PL\" Width=\"6\" Height=\"2\" Fill=\"#FFB6BEC6\" Canvas.Left=\"46\" Canvas.Top=\"10\" /><Rectangle Tag=\"LED_MD\" Width=\"6\" Height=\"2\" Fill=\"#FFB6BEC6\" Canvas.Left=\"38\" Canvas.Top=\"10\" /></Canvas><Canvas Tag=\"Slot\" IsHitTestVisible=\"False\"><Path Tag=\"Right_Notch\" Fill=\"#FFD8DCE1\" Canvas.Left=\"68.999\" Canvas.Top=\"110.084\"><Path.Data><PathGeometry Figures=\"M0 6.085v49c0 3.793 7 11.093 7 11.093V0S0 1.823 0 6.085Z\" /></Path.Data><Path.RenderTransform><RotateTransform CenterX=\"0\" CenterY=\"0\" Angle=\"180\" /></Path.RenderTransform></Path><Path Tag=\"Left_Notch\" Fill=\"#FFD8DCE1\" Canvas.Left=\"-11813\" Canvas.Top=\"-14960.092\"><Path.Data><PathGeometry Figures=\"M11816 15064.091v-49c0-3.793 7-11.093 7-11.093v66.176S11816 15068.353 11816 15064.091Z\" /></Path.Data></Path><Canvas Tag=\"Frame\"><Rectangle Width=\"53.0\" Height=\"110.0\" StrokeThickness=\"1\" Canvas.Left=\"9.5\" Canvas.Top=\"15.5\" /><Rectangle Width=\"52.0\" Height=\"109.0\" Stroke=\"#FFD8DCE1\" StrokeThickness=\"1\" Canvas.Left=\"9.5\" Canvas.Top=\"15.5\" /></Canvas></Canvas><Rectangle Tag=\"ModulePlaceHolder\" Width=\"50\" Height=\"107\" Canvas.Left=\"11\" Canvas.Top=\"17\" IsHitTestVisible=\"False\" attachedProperties:SuiteProps.TranslateTransformX=\"11\" attachedProperties:SuiteProps.TranslateTransformY=\"17\" attachedProperties:SuiteProps.ModulePlaceHolder=\"\" /><Canvas Tag=\"ApAddressPlaceHolder\" IsHitTestVisible=\"False\" attachedProperties:SuiteProps.TranslateTransformX=\"118\" attachedProperties:SuiteProps.TranslateTransformY=\"128\" attachedProperties:SuiteProps.IsApAddressPlaceHolder=\"True\"><Rectangle Tag=\"ApAddressPlaceHolder-2\" Width=\"20\" Height=\"8\" Fill=\"#FFFFFFFF\" Canvas.Left=\"118\" Canvas.Top=\"128\" /></Canvas></Canvas>";
-            string placeHolderText = "ModulePlaceHolder=\"";
 
-            var index = iconMarkup.IndexOf(placeHolderText);
-            if (index == -1)
-                return;
-            if (iconMarkup.Substring(index + placeHolderText.ToCharArray().Length, 1) == "\"") 
-            {
-                // empty value
-                var placeHolderFilled = placeHolderText + Guid.NewGuid();
-                iconMarkup =  iconMarkup.Replace(placeHolderText, placeHolderFilled);
+            var res = SetSlotInIdForPlaceholder(iconMarkup, Guid.NewGuid().ToString());
+            res = SetSlotInIdForPlaceholder(res, null);
 
-            }
+            res = SetSlotInIdForPlaceholder(res, null);
         }
 
         private static void ZipArchive()
