f66544108cbce8198037646c84786c0ea1bb7349
Drachnev, Alexander <alexander.drachnev@festo.com>
2025-01-29 15:37:16 +0100

    catalog RAM  test

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Festo.ComponentData.Contract;
+using Festo.ComponentData.Json;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,6 +26,8 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CatalogData2 _deviceCatalog;
+
         public MainWindow()
         {
              
@@ -33,8 +40,46 @@ namespace WpfApp1
             {   
                 DragDrop.SetDragHandler(listViewCatalogItems, new DragHandler(listViewCatalogItems));
             }
+
+
+            Func<CatalogData2> getCatalog = () => DeviceCatalog;
+            Action resetCatalog = () => DeviceCatalog = null;
+
+
+
+            var item = getCatalog().CatalogItems.First();
+
+
+
+            resetCatalog();
+
+
+            item = getCatalog().CatalogItems.First();
+
         }
 
+        
+
+        public CatalogData2 DeviceCatalog
+        {
+            get
+            {
+                if (_deviceCatalog == null)
+                {
+                    var manifestAssembly = Assembly.GetExecutingAssembly();
+                    var catalogFile = "WpfApp1.catalog.json";
+
+                    using (var reader = new StreamReader(manifestAssembly.GetManifestResourceStream(catalogFile), System.Text.Encoding.UTF8))
+                    {
+                        var catalogJson = reader.ReadToEnd();
+                        _deviceCatalog = JsonConvert.DeserializeObject<CatalogData2>(catalogJson, ComponentDataJsonSettings.Default);
+                    }
+                }
+
+                return _deviceCatalog;
+            }
+            private set { _deviceCatalog = value; }
+        }
 
         private void JogLeftButton_OnClick(object sender, RoutedEventArgs e)
         {
